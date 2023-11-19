22a3edf483be8b8c8034648a8b2ed8692ac0e4c9
Dmitry Ulitin <dmitry.ulitin@gmail.com>
2023-11-19 11:58:37 +0200

    move all auth code to ./auth folder

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,7 +5,7 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { TuiAlertModule, TuiRootModule } from '@taiga-ui/core';
-import { jwtInterceptor } from './jwt.interceptor';
+import { jwtInterceptor } from './auth/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideHttpClient(withInterceptors([jwtInterceptor])), provideAnimations(), importProvidersFrom(TuiRootModule), importProvidersFrom(TuiAlertModule)]
