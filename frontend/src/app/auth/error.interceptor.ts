465e13e97295075a2d4905fd91fe5202d85d5f60
Dmitry Ulitin <dmitry.ulitin@gmail.com>
2024-01-19 20:38:07 +0200

    rethrow error

--- frontend/src/app/auth/error.interceptor.ts
+++ frontend/src/app/auth/error.interceptor.ts
@@ -5,12 +5,12 @@ import { DataService } from '../services/data.service';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const data = inject(DataService);
-  return next(req).pipe(catchError((err, caught) => {
+  return next(req).pipe(catchError((err) => {
     if (err.status === 403) {
       // auto logout if 403 response returned from api
       data.reset();
       return EMPTY;
     }
-    return caught;
+    throw err;
   }));;
 };
