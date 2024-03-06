cff6673ce10c180081cb40bb7835e881eb9b9bc6
Dmitry Ulitin <dmitry.ulitin@gmail.com>
2024-03-06 17:50:42 +0200

    add i18n

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,7 +7,12 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { TuiAlertModule, TuiRootModule } from '@taiga-ui/core';
 import { jwtInterceptor } from './auth/jwt.interceptor';
 import { errorInterceptor } from './auth/error.interceptor';
+import localeRu from '@angular/common/locales/ru';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeRu, 'ru');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withInterceptors([jwtInterceptor, errorInterceptor])), provideAnimations(), importProvidersFrom(TuiRootModule), importProvidersFrom(TuiAlertModule)]
+  providers: [provideRouter(routes), provideHttpClient(withInterceptors([jwtInterceptor, errorInterceptor])),
+    provideAnimations(), importProvidersFrom(TuiRootModule), importProvidersFrom(TuiAlertModule), { provide: LOCALE_ID, useValue: 'ru' }]
 };
