aad335d15217e2b7d9cab560273199bb6b73d2f2
Dmitry Ulitin <dmitry.ulitin@gmail.com>
2024-10-21 14:28:35 +0300

    feat: Add support for Unicredit bank in import process and update bank list

--- frontend/src/app/load/load.stat.component.ts
+++ frontend/src/app/load/load.stat.component.ts
@@ -14,7 +14,8 @@ import { Subject } from 'rxjs';
   styleUrl: './load.stat.component.scss'
 })
 export class LoadStatComponent {
-  readonly banks = [{ id: 1, name: 'LHV', accept: ".csv" }, { id: 2, name: 'Tinkoff', accept: ".csv" }, { id: 3, name: 'Сбербанк', accept: ".pdf" }, { id: 4, name: 'Альфа-Банк', accept: ".xlsx" }];
+  readonly banks = [{ id: 1, name: 'LHV', accept: ".csv" }, { id: 2, name: 'Tinkoff', accept: ".csv" }, { id: 3, name: 'Сбербанк', accept: ".pdf" },
+    { id: 4, name: 'Альфа-Банк', accept: ".xlsx" }, { id: 5, name: 'Unicredit', accept: ".xls" }];
   readonly files = new FormControl();
   readonly bank = new FormControl(this.banks[0], { nonNullable: true, validators: [Validators.required] });
   readonly rejectedFiles$ = new Subject<TuiFileLike | null>();
