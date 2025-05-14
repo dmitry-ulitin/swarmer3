51a1d5caac153778ba71cf49cb3c6b8526c1b9f8
Dmitry Ulitin <dmitry.ulitin@gmail.com>
2025-05-14 17:47:39 +0300

    feat: add change password functionality with UI components
    - Implemented ChangePasswordComponent for user password changes.
    - Added routing for change password page in app.routes.ts.
    - Updated header to include a link to change password.
    - Enhanced login and registration forms with improved UI components.
    - Updated AuthService to handle change password requests.
    - Improved error handling in AlertService for better user feedback.
    - Updated package dependencies for UI components to latest versions.

--- frontend/src/app/auth/registration.component.ts
+++ frontend/src/app/auth/registration.component.ts
@@ -1,20 +1,18 @@
-import { TuiTextfieldControllerModule, TuiComboBoxModule, TuiInputModule, TuiInputPasswordModule } from "@taiga-ui/legacy";
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { AlertService } from '../services/alert.service';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { TuiButton, TuiLabel } from '@taiga-ui/core';
-import { TuiDataListWrapper, TuiFilterByInputPipe } from '@taiga-ui/kit';
+import { TuiButton, TuiIcon, TuiLabel, TuiTextfield, TuiTextfieldDropdownDirective } from '@taiga-ui/core';
+import { TuiDataListWrapper, TuiFilterByInputPipe, TuiPassword } from '@taiga-ui/kit';
 import { CURRENCIES_EN } from '../models/currencies';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, ReactiveFormsModule, RouterLink, TuiButton, TuiInputModule, TuiInputPasswordModule,
-            TuiLabel, TuiTextfieldControllerModule, TuiComboBoxModule, TuiDataListWrapper, TuiFilterByInputPipe],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, TuiButton, TuiTextfield, TuiLabel, TuiIcon, TuiPassword, TuiDataListWrapper, TuiFilterByInputPipe, TuiTextfieldDropdownDirective],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
@@ -36,7 +34,7 @@ export class RegistrationComponent {
 
   async onRegister() {
     try {
-      await this.#auth.register({...this.form.value, currency: this.form.value.currency?.toUpperCase(), name: this.form.value.name || this.defaultName});
+      await this.#auth.register({ ...this.form.value, currency: this.form.value.currency?.toUpperCase(), name: this.form.value.name || this.defaultName });
     } catch (err) {
       this.#alerts.printError(err);
     }
